8c5df53cb7ac7afc8f5c84c1288f6d4de2349a92
McTintor <igortintor99@gmail.com>
2024-11-01 17:18:45 +0100

    added starting components and implemented food display

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logoImg from '../assets/logo.jpg'
+import Button from './UI/Button'
+
+export default function Header() {
+    return (
+        <>
+            <header id="main-header">
+                <div id="title">
+                    <img src={logoImg} alt="Restaurant Logo" />
+                    <h1>ReactFood</h1>
+                </div>
+                <nav>
+                    <Button textOnly>Cart ()</Button>
+                </nav>
+            </header>
+        </>
+    )
+}
